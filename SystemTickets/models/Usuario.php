06370979849edf5a9eea5bf029f19f0a0b9ca671
J-E-Calderon <juan.eduardo.calderon@correounivalle.edu.co>
2024-05-26 14:58:45 -0500

    tags

--- SystemTickets/models/Usuario.php
+++ SystemTickets/models/Usuario.php
@@ -4,25 +4,25 @@
             $conectar = parent::conexion();
             parent::set_Names();
             if(isset($_POST["enviar"])){
-                $correo = $_POST["emp_email"];
-                $dni = $_POST["emp_dni"];
-                if (empty($correo) or empty($dni)) {
+                $correo = $_POST["email_emp"];
+                $password = $_POST["password_emp"];
+                if (empty($correo) or empty($password)) {
                     header ("Location:".Conexion::ruta()."index.php?m=2");
                     exit();
                 }
                 else{
-                    $sql = "SELECT * FROM employees WHERE emp_email=? AND emp_dni=? AND statusEmp=1";
+                    $sql = "SELECT * FROM empleados WHERE email_emp=? AND password_emp=?";
                     $stmt = $conectar->prepare($sql);
                     $stmt -> bindValue (1, $correo);
-                    $stmt -> bindValue (2, $dni);
+                    $stmt -> bindValue (2, $password);
                     $stmt -> execute();
                     $resultado = $stmt -> fetch();
                     if(is_array($resultado) and count($resultado)>0){
                         // Atributos de la tabla empleados
-                        $_SESSION["emp_id"] = $resultado["emp_id"];
-                        $_SESSION["emp_dni"] = $resultado["emp_dni"];
-                        $_SESSION["emp_email"] = $resultado["emp_email"];
-                        $_SESSION["emp_name"] = $resultado["emp_name"];
+                        $_SESSION["Id_emp"] = $resultado["Id_emp"];
+                        $_SESSION["DNI_emp"] = $resultado["DNI_emp"];
+                        $_SESSION["email_emp"] = $resultado["email_emp"];
+                        $_SESSION["nombre_emp"] = $resultado["nombre_emp"];
                         header("Location: ".Conexion::ruta()."view/home/index.php");
                         exit();
                     }
@@ -34,5 +34,5 @@
             }
         }
     }
-
+    
 ?>
